271dded28fab1edc7e94496c2568671ae970c013
oceanumeric <numerical.ocean@gmail.com>
2023-04-15 19:34:18 +0200

    [update] the project

--- basics/activation_fun.py
+++ basics/activation_fun.py
@@ -1,31 +1,28 @@
 # %%
+# import packages that are not related to torch
 import os
-import json
 import math
+import time
+import inspect
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
-import urllib.request
-from urllib.error import HTTPError
+import matplotlib.pyplot as plt
 
-# pytorch imports
+
+# torch import
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as tu_data
-import torch.optim as optim
-import torchvision
-from torchvision.datasets import FashionMNIST
 from torchvision import transforms
+from torchvision.datasets import FashionMNIST
 
 
-# set seaborn style
-sns.set()
-# set up path
-DATASET_PATH = "../data"
-# checkpoint path
-CHECKPOINT_PATH = "../checkpoints"
+### --------- environment setup --------- ###
+# set up the data path
+DATA_PATH = "../data"
+
 
 # function for setting seed
 def set_seed(seed):
@@ -34,7 +31,9 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-        
+
+
+# set up seed globally and deterministically
 set_seed(76)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -43,357 +42,176 @@ torch.backends.cudnn.benchmark = False
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class ActivationFunction(nn.Module):
-    
+### --------- data preprocessing --------- ###
+
+
+def _get_data():
+    """
+    download the dataset from FashionMNIST and transfom it to tensor
+    """
+    # set up the transformation
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+
+    # download the dataset
+    train_set = FashionMNIST(
+        root="./data", train=True, download=True, transform=transform
+    )
+    test_set = FashionMNIST(
+        root="./data", train=False, download=True, transform=transform
+    )
+
+    return train_set, test_set
+
+
+def _visualize_data(train_dataset, test_dataset):
+    """
+    visualize the dataset by randomly sampling
+    9 images from the dataset
+    """
+    # set up the figure
+    fig = plt.figure(figsize=(8, 8))
+    col, row = 3, 3
+
+    for i in range(1, col * row + 1):
+        sample_idx = np.random.randint(0, len(train_dataset))
+        img, label = train_dataset[sample_idx]
+        fig.add_subplot(row, col, i)
+        plt.title(train_dataset.classes[label])
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+
+
+## build up a class for different activation functions
+class AcFun(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(AcFun, self).__init__()
         self.name = self.__class__.__name__
         self.config = {"name": self.name}
-        
 
-# set up activation functions
-class Sigmoid(ActivationFunction):
-    
+
+# inherit from AcFun
+class Sigmoid(AcFun):
+    # it is important to know that pytorch class
+    # has __call__ function, which calls the forward automatically
+    # therefore when you intialize the class, you are calling
+    # the forward function directly
     def forward(self, x):
-        return 1/(1+torch.exp(-x))
-    
+        return torch.sigmoid(x)
 
-class Tanh(ActivationFunction):
-    
+
+class Tanh(AcFun):
     def forward(self, x):
-        exp_x, exp_neg_x = torch.exp(x), torch.exp(-x)
-        return (exp_x - exp_neg_x)/(exp_x + exp_neg_x)
-    
+        return torch.tanh(x)
 
-class ReLU(ActivationFunction):
-    
+
+class ReLU(AcFun):
     def forward(self, x):
-        return x * (x > 0).float()
-    
+        return torch.relu(x)
 
-class LeakyReLU(ActivationFunction):
-        
-        def __init__(self, alpha=0.1):
-            super().__init__()
-            self.config["alpha"] = alpha
-            
-        def forward(self, x):
-            return torch.where(x > 0, x, self.config["alpha"]*x)
-        
 
-class ELU(ActivationFunction):
-    
+class LeakyReLU(AcFun):
+    def __init__(self, negative_slope=0.1):
+        super(LeakyReLU, self).__init__()
+        self.negative_slope = negative_slope
+        self.config["negative_slope"] = negative_slope
+
     def forward(self, x):
-        return torch.where(x > 0, x, torch.exp(x)-1)
-    
+        return F.leaky_relu(x, self.negative_slope)
 
-class Swish(ActivationFunction):
-    
+
+class ELU(AcFun):
+    def forward(self, x):
+        return F.elu(x)
+
+
+class Swish(AcFun):
     def forward(self, x):
         return x * torch.sigmoid(x)
 
 
-# create a dictionary of activation functions
-activation_fun_dict = {
-    "sigmoid": Sigmoid,
-    "tanh": Tanh,
-    "ReLU": ReLU,
-    "LeakyReLU": LeakyReLU,
-    "elu": ELU,
-    "swish": Swish
-}
-
-
-def get_grads(activation_fun, x):
+# function to get gradient of the activation function
+def _get_grad(ac_fun, x):
     """
-    Compute gradients of activation function based on input x
-    
-    Inputs:
-        activation_fun: activation function
-        x: 1D tensor
-    Output:
-        1D tensor of gradients
+    get the gradient of the activation function at x
     """
-    # intialize input vector with greadients
-    x = x.clone().requires_grad_(True)
-    out = activation_fun(x)
-    # calculate gradients
-    out.sum().backward()
-    
+    # copy the input tensor and set requires_grad to True
+    x = x.clone().detach().requires_grad_(True)
+    y = ac_fun(x)
+    # slower version y.backward(torch.ones_like(y))
+    y.sum().backward()  # faster version
     return x.grad
 
 
-def visualize_activation_funs(act_fun, x, ax):
-    y = act_fun(x)
-    y_grads = get_grads(act_fun, x)
-    # push to cpu
-    x, y, y_grads = x.cpu().numpy(), y.cpu().numpy(), y_grads.cpu().numpy()   
-    # plot activation function
-    ax.plot(x, y, label = act_fun.config["name"], linewidth=2)
-    ax.plot(x, y_grads, label = act_fun.config["name"] + " grads", linewidth=2)
-    ax.set_title(act_fun.name)
+def _vis_grad(ac_fun, x, ax):
+    """
+    visualize the gradient of the activation function
+    Input:
+        ac_fun: activation function
+        x: input tensor
+        ax: matplotlib axis
+    """
+    # calculate the output
+    y = ac_fun(x)
+    # get the gradient
+    grad = _get_grad(ac_fun, x)
+
+    # pass the data to cpu and convert to numpy
+    x, y, grad = x.cpu().numpy(), y.cpu().numpy(), grad.cpu().numpy()
+    # plot the gradient
+    ax.plot(x, y, "k-", label="AcFun")
+    ax.plot(x, grad, "k--", label="Gradient")
+    ax.set_title(ac_fun.name)
     ax.legend()
     ax.set_ylim(-1.5, x.max())
-    
-
-def download_pretrained_models():
-    base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/tutorial3/"
-    pretrained_files = ["FashionMNIST_elu.config", "FashionMNIST_elu.tar",
-                    "FashionMNIST_leakyrelu.config", "FashionMNIST_leakyrelu.tar",
-                    "FashionMNIST_relu.config", "FashionMNIST_relu.tar",
-                    "FashionMNIST_sigmoid.config", "FashionMNIST_sigmoid.tar",
-                    "FashionMNIST_swish.config", "FashionMNIST_swish.tar",
-                    "FashionMNIST_tanh.config", "FashionMNIST_tanh.tar"]
-    # Create checkpoint path if it doesn't exist yet
-    os.makedirs(CHECKPOINT_PATH, exist_ok=True)
-
-    # For each file, check whether it already exists. If not, try downloading it.
-    for file_name in pretrained_files:
-        file_path = os.path.join(CHECKPOINT_PATH, file_name)
-        if not os.path.isfile(file_path):
-            file_url = base_url + file_name
-            print(f"Downloading {file_url}...")
-            try:
-                urllib.request.urlretrieve(file_url, file_path)
-            except HTTPError as e:
-                print("Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n", e)
-    
-
-# set up a network
-class BaseNet(nn.Module):
-    
-    def __init__(self, activation_fun,
-                 input_dim = 784,
-                 output_dim = 10,
-                 hidden_dims = [512, 256, 256, 128]):
-        """
-        Inputs:
-            activation_fun: activation function
-            input_dim: input dimension of images as flattened vector
-            output_dim: output dimension of network (number of classes)
-            hidden_dims: list of hidden dimensions (4 hidden layers)
-        
-        we have defined default values for input_dim, output_dim
-        and hidden_dims, but you can change them if you want
-        """
-        super().__init__()
-        # build up network in the loop
-        # we can do this because each layer has the same structure
-        layers = []
-        layer_sizes = [input_dim] + hidden_dims
-        
-        for i in range(1, len(layer_sizes)):
-            # all layers are linear except the last one
-            layers.append(nn.Linear(layer_sizes[i-1], layer_sizes[i]))
-            # liear layers are followed by activation function
-            layers.append(activation_fun)
-        # add last linear layer
-        layers.append(nn.Linear(layer_sizes[-1], output_dim))
-        
-        self.layers = nn.Sequential(*layers)
-        
-        # save config
-        self.config = {
-            "activation_fun": activation_fun.config,
-            "input_dim": input_dim,
-            "output_dim": output_dim,
-            "hidden_dims": hidden_dims
-        }
-    
-    def forward(self, x):
-        # flatten images
-        x = x.view(x.shape[0], -1)
-        return self.layers(x)
 
 
-def _get_config_file(model_path, model_name):
-    # Name of the file for storing hyperparameter details
-    return os.path.join(model_path, model_name + ".config")
-
-
-def _get_model_file(model_path, model_name):
-    # Name of the file for storing network parameters
-    return os.path.join(model_path, model_name + ".tar")
-
-
-def load_model(model_path, model_name, net=None):
+def vis_ac_fun(ac_fun_dict):
     """
-    Load a model from a file.
-    
-    Inputs:
-        model_path: path to the folder containing the model
-        model_name: name of the model (without extension)
-    Output:
-        model: the loaded model
+    visualize the activation function and its gradient
     """
-    # Load the config file
-    config_file = _get_config_file(model_path, model_name)
-    assert os.path.isfile(config_file), f"Config file {config_file} does not exist"
-    with open(config_file, "r") as f:
-        config = json.load(f)
-    
-    # Load the model
-    model_file = _get_model_file(model_path, model_name)
-    assert os.path.isfile(model_file), f"Model file {model_file} does not exist"
-    
-    if net is None:
-        act_fn_name = config["act_fn"].pop("name").lower()
-        act_fn = activation_fun_dict[act_fn_name](**config.pop("act_fn"))
-        net = BaseNet(act_fn=act_fn, **config)
-    net.load_state_dict(torch.load(model_file, map_location=device))
-    return net
 
+    # get number of activation functions
+    num = len(ac_fun_dict)
+    columns = math.ceil(num / 2)
 
-def save_model(model, model_path, model_name):
-    """
-    Given a model, we save the state_dict and hyperparameters.
+    # initialize the input tensor
+    x = torch.linspace(-5, 5, 1000)
 
-    Inputs:
-        model - Network object to save parameters from
-        model_path - Path of the checkpoint directory
-        model_name - Name of the model (str)
-    """
-    config_dict = model.config
-    os.makedirs(model_path, exist_ok=True)
-    config_file, model_file = _get_config_file(model_path, model_name), _get_model_file(model_path, model_name)
-    with open(config_file, "w") as f:
-        json.dump(config_dict, f)
-    torch.save(model.state_dict(), model_file)
-    
-    
-def visualize_gradients(net, color="c0"):
-    """
-    Inputs:
-        net: network, class object
-        color: color of the plot
-    remark: this function is designed to be called in a loop
-    """
-    # we are loading a pretrained model, so we don't need to train it
-    # we only need to evaluate it
-    net.eval()
-    small_loader = tu_data.DataLoader(train_set, batch_size=256,
-                                      shuffle=False)
-    imgs, labels = next(iter(small_loader))
-    imgs, labels = imgs.to(device), labels.to(device)
-    
-    # pass images through the network
-    net.zero_grad()  # reset gradients
-    preds = net(imgs)  # forward pass and compute predictions
-    loss = F.cross_entropy(preds, labels)  # compute loss
-    loss.backward()  # compute gradients
-    
-    # extract graidients for weights but not for biases
-    grads = {name: param.grad.data.view(-1).cpu().clone().numpy() for name,
-                param in net.named_parameters()
-                if "weight"  in name}
-    net.zero_grad()  # reset gradients
-    
-    ## Plotting
-    columns = len(grads)
-    fig, ax = plt.subplots(1, columns, figsize=(columns*3.5, 2.5))
-    fig_index = 0
-    for key in grads:
-        key_ax = ax[fig_index%columns]
-        sns.histplot(data=grads[key], bins=30, ax=key_ax, color=color, kde=True)
-        key_ax.set_title(str(key))
-        key_ax.set_xlabel("Grad magnitude")
-        fig_index += 1
-    fig.suptitle(f"Gradient magnitude distribution for activation function {net.config['activation_fun']['name']}", fontsize=14, y=1.05)
-    fig.subplots_adjust(wspace=0.45)
-    plt.show()
-    plt.close()
-    
+    # set up the figure
+    fig, axes = plt.subplots(2, columns, figsize=(4 * columns, 8))
+    axes = axes.flatten()
+    for i, ac_fun in enumerate(ac_fun_dict.values()):
+        _vis_grad(ac_fun, x, axes[i])
+    fig.subplots_adjust(hspace=0.3)
 
-def train_model(net, model_name, max_epochs=50, patience=7,
-                    batch_size=256, overwrite=False):
-    """
-    Train a model on the training set of FashionMNIST
-
-    Inputs:
-        net - Object of BaseNetwork
-        model_name - (str) Name of the model, used for creating the checkpoint names
-        max_epochs - Number of epochs we want to (maximally) train for
-        patience - If the performance on the validation set has not
-                    improved for #patience epochs, we stop training early
-        batch_size - Size of batches used in training
-        overwrite - Determines how to handle the case when there already
-                    exists a checkpoint. If True, it will be overwritten.
-                    Otherwise, we skip training.
-    """
-    file_exists = os.path.isfile(_get_model_file(CHECKPOINT_PATH, model_name))
-    if file_exists and not overwrite:
-        print("Model file already exists. Skipping training...")
-    else:
-        if file_exists:
-            print("Model file exists, but will be overwritten...")
-    
-    
-        
-    
 
 if __name__ == "__main__":
     print(os.getcwd())
-    print("Cuda version: ", torch.version.cuda)
-    print("Using device: ", device)
-    
-    # download pretrained models
-    # download_pretrained_models()
-    
-    # visualize activation functions
-    # act_fns = [act_fn() for act_fn in activation_fun_dict.values()]
-    # x = torch.linspace(-5, 5, 1000)
-    # fig_rows = math.ceil(len(act_fns)/2)
-    # fig, ax = plt.subplots(fig_rows, 2, figsize=(8, fig_rows*4))
-    # for i, act_fun in enumerate(act_fns):
-    #     visualize_activation_funs(act_fun, x, ax[divmod(i, 2)])
-    # fig.subplots_adjust(hspace=0.3)
-    
-    # download data
-    transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,), (0.5,))])
-    train_dataset = FashionMNIST(root=DATASET_PATH, train=True,
-                                 download=True, transform=transform)
-    train_set, val_set = tu_data.random_split(train_dataset,
-                                                       [50000, 10000])
+    print("Using torch", torch.__version__)
+    print("Using device", device)
+    # set up finger config
+    # it is only used for interactive mode
+    %config InlineBackend.figure_format = 'retina'
 
-    # Loading the test set
-    test_set = FashionMNIST(root=DATASET_PATH, train=False,
-                            transform=transform,
-                            download=True)
-    train_loader = tu_data.DataLoader(train_set,
-                                   batch_size=1024,
-                                   shuffle=True,
-                                   drop_last=False)
-    val_loader = tu_data.DataLoader(val_set,
-                                 batch_size=1024,
-                                 shuffle=False,
-                                 drop_last=False)
-    test_loader = tu_data.DataLoader(test_set,
-                                  batch_size=1024,
-                                  shuffle=False,
-                                  drop_last=False)
-    
-    # check images
-    # exmp_imgs = [train_set[i][0] for i in range(16)]
-    # # Organize the images into a grid for nicer visualization
-    # img_grid = torchvision.utils.make_grid(torch.stack(exmp_imgs, dim=0),
-    #                                        nrow=4,
-    #                                        normalize=True,
-    #                                        pad_value=0.5)
-    # img_grid = img_grid.permute(1, 2, 0)
+    # download the dataset
+    # train_set, test_set = _get_data()
+    # _visualize_data(train_set, test_set)
 
-    # plt.figure(figsize=(8,8))
-    # plt.title("FashionMNIST examples")
-    # plt.imshow(img_grid)
-    # plt.axis('off')
-    # plt.show()
-    # plt.close()
-    
-    # plot gradient distribution
-    # for i, act_fn_name in enumerate(activation_fun_dict):
-    #     set_seed(716) # Setting the seed ensures that we have the same weight initialization for each activation function
-    #     act_fn = activation_fun_dict[act_fn_name]()
-    #     net_actfn = BaseNet(activation_fun=act_fn).to(device)
-    #     visualize_gradients(net_actfn, color=f"C{i}")
-    
+    # create a dictionary of activation functions
+    # 6 activation functions
+    ac_fun_dict = {
+        "Sigmoid": Sigmoid(),
+        "Tanh": Tanh(),
+        "ReLU": ReLU(),
+        "LeakyReLU": LeakyReLU(),
+        "ELU": ELU(),
+        "Swish": Swish(),
+    }
+    # set seaborn style
+    sns.set_style("ticks")
+    vis_ac_fun(ac_fun_dict)
+
+
 # %%
---
+++ basics/activation_functions.md
@@ -0,0 +1,14 @@
+# Activation Functions 
+
+
+## Introduction 
+
+After the innovation like ChatGPT, I begun to think about the fundamentals of deep learning models. In the old days (I mean three to five years ago), I had some doubt on deep learning models as there is lack of theoretical support. Compared to traditional machine learning and statistcal models, deep learning models are more like black boxes. However, somehow it works and it is creating a lot of impact in the industry.
+
+Therefore, I updated my perception on deep learning models. Instread of caring a lot about the theoretical support, I think it is more important to understand the mechanism of deep learning models. It is better to think about the deep learning models as a nerual network architecture that could map data from one space to another space. 
+
+With this perspective, one could understand how two architectures have changed our world. One is the word2vec model, which maps words from one space to another space. The other is the transformer model, which maps sentences from one space to another space. As the famous paper stated, the attention is all we need. 
+
+In this article, I will talk about the activation functions. Since activation functions are the core of deep learning models, I think it is important to understand the mechanism of activation functions.
+
+
